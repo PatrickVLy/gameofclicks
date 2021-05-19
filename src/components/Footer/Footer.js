4b3781b42dabc793494b979327c1033599241d9f
Patrick Ly <patrickly72@gmail.com>
2021-05-18 22:17:46 -0700

    added footer compnent

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+// IMPORTS
+import React from 'react';
+import "./Footer.css";
+// PROPS: Footer
+const Footer = props => (
+    <table>
+    </table>
+)
+// EXPORT DEFAULT: Footer
+export default Footer;
