5693f3de99fab9fdf1b4a89c9b9892099fa74e68
Patrick Ly <patrickly72@gmail.com>
2021-05-18 22:15:08 -0700

    added index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// IMPORTS
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+
+// RENDER 
+ReactDOM.render(<App />, document.getElementById('root'));
