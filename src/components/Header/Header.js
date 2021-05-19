b8f7dc589c48cec71435820ea8c1f788e4fe43e6
Patrick Ly <patrickly72@gmail.com>
2021-05-18 22:17:12 -0700

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+// IMPORTS
+import React from 'react';
+import "./Header.css";
+// PROPS: Header
+const Header = props => (
+    <header className="header row">
+        <div className="col-lg-3"></div>
+        <div className="col-lg-6 text-center">
+        <h1>Game of Clicks</h1>
+        </div>
+        <div className="col-lg-3"></div>
+    </header>
+)
+// EXPORT DEFAULT: Header
+export default Header;
